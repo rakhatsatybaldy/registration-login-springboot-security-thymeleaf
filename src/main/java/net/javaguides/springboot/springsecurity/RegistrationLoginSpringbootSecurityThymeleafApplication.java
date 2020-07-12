9e1d54dad8beec6a4ae57eb847bffdfe01668f17
rakhatsatybaldy <rahat_05_01@mail.ru>
2020-07-12 22:19:31 +0600

    Initial commit

---
+++ src/main/java/net/javaguides/springboot/springsecurity/RegistrationLoginSpringbootSecurityThymeleafApplication.java
@@ -0,0 +1,14 @@
+package net.javaguides.springboot.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+public class RegistrationLoginSpringbootSecurityThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationLoginSpringbootSecurityThymeleafApplication.class, args);
+	}
+
+}
